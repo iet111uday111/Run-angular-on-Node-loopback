ea7c86a7a6541cd6797c11bbefd992a500819ce1
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-04 16:38:59 +0530

    Run-angular-on-Node-loopback

---
+++ server/server.js
@@ -0,0 +1,50 @@
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+
+var app = module.exports = loopback();
+var path = require('path');
+
+// boot scripts mount components like REST API
+const allowedExt = [
+  '.js',
+  '.ico',
+  '.css',
+  '.png',
+  '.jpg',
+  '.woff2',
+  '.woff',
+  '.ttf',
+  '.svg',
+  '.TTF',
+  '.ttf',
+  '.eot',
+];
+
+boot(app, __dirname);
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
+};
+
+// start the server if `$ node server.js`
+if (require.main === module) {
+  app.start();
+}
+
+app.get('*', (req, res) => {
+  if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+    res.sendFile(path.resolve(`client/${req.url}`));
+  } else {
+    res.sendFile(path.resolve('client/index.html'));
+  }
+});
+
